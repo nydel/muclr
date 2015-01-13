69725c9ff25d3124383746530cbc2aca8eb25901
miercoledi <github.com@meerkat.cc>
2015-01-13 01:00:45 -0800

    data annex

--- data/credentials.lisp
+++ data/credentials.lisp
@@ -5,7 +5,7 @@
 
 (in-package :muclr-credentials)
 
-(defparameter *db-credentials-path* #P"credentials.db")
+(defparameter *db-credentials-path* #P"data/credentials.db")
 (defvar *db-credentials* nil)
 
 (defclass user-credential ()
@@ -20,14 +20,14 @@
 	 :accessor user-credential-hash)))
 
 (defun split-user-credential (uc)
-  (format t "~a ~a ~a"
+  (format t "~a,~a,~a"
 	  (user-credential-uid uc)
 	  (user-credential-username uc)
 	  (user-credential-hash uc)))
 
 (defun &load-credentials-db ()
   (with-open-file (@db *db-credentials-path* :direction :input)
-    (loop for line = (read @db nil 'eof)
+    (loop for line = (read-line @db nil 'eof)
        until (equal line 'eof)
 	 collect line)))
 
@@ -38,7 +38,8 @@
   (with-open-file (@db *db-credentials-path* :direction :output
 		       :if-exists :rename :if-does-not-exist :create)
     (when *db-credentials*
-      (format @db "~{~S~}" *db-credentials*))))
+      (loop for i in *db-credentials* do
+	   (format @db "~{~a~}" (mapcar (lambda (y) (split-user-credentials y)) *db-credentials*))))))
 
 (defun digest-hash (string &key digest)
   (ironclad:byte-array-to-hex-string
@@ -58,5 +59,14 @@
 			   :username username
 			   :pw pw)
    *db-credentials*))
+
+(defun &rebuild-user-credential (string)
+  (split "\\," string))
+
+;(defun rebuild-user-credentials (string)
+;  (let ((creds (&rebuild-user-credential string)))
+;    (build-user-credential :uid (first creds)
+;			   :username (second creds)
+;			   :pw
 
 (defun init-credentials ())
