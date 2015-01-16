ca7c76c31e482a68c31fc1f6bb90c8e6e918b58b
nydel <nydel@ma.sdf.org>
2015-01-16 17:26:21 -0600

    comment

--- load.lisp
+++ load.lisp
@@ -1,2 +1,5 @@
+;; this simple file enables the user to just do (load "load") instead of typing
+;; the following two forms out for evaluation every single time
+
 (asdf::load-asd "muclr.asd")
 (asdf:load-system :muclr)
